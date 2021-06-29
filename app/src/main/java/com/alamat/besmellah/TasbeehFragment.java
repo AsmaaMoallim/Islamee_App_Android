8fa4115fd6f0cce4b753ecab9e2184e801c6407a
Asmaa <asmaaam1o1@gmail.com>
2021-06-29 06:40:23 +0300

    fixing tasbeeh logic, adding the strings to both recycler views in the app

--- app/src/main/java/com/alamat/besmellah/TasbeehFragment.java
+++ app/src/main/java/com/alamat/besmellah/TasbeehFragment.java
@@ -48,7 +48,7 @@ public class TasbeehFragment extends Fragment implements AdapterView.OnItemSelec
                 try {
 //                    counter += 1;
                     counterArr.set(index, counter++);
-                    binding.tvTasbeesCounterView.setText("العدد = " + counter);
+                    setTextsvalues(index);
                 } catch (IndexOutOfBoundsException e) {
                     return;
                 }
@@ -61,36 +61,63 @@ public class TasbeehFragment extends Fragment implements AdapterView.OnItemSelec
             public void onClick(View v) {
                 counter = 0;
                 counterArr.add(index, 0);
-                binding.tvTasbeesCounterView.setText("العدد = " + counterArr.get(index));
-            }
+                setTextsvalues(index);            }
         });
 
         binding.btnTasbeehRefreshAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter = 0;
-                for (int i = 1; i < spinnerValues.length; i++) {
-                    counterArr.add(i, 0);
+                for (int i = 0; i < spinnerValues.length; i++) {
+                    counterArr.set(i, counter);
+                    setTextsvalues(i);
                 }
-                binding.tvTasbeesCounterView.setText("العدد = " + counterArr.get(index));
             }
+
         });
         ////////////////////////////////////////////////////////////////////////////////
 
         for (int i = 0; i < spinnerValues.length; i++) {
             counterArr.add(0);
         }
+
         return view;
     }
 
     String[] spinnerValues = {"سبحان الله", "الحمد لله", "استغفر الله", "لا إله إلا الله", "سبحان الله العظيم"};
 
 
+    public void setTextsvalues(int index){
+            switch (index) {
+                case 0:
+                    binding.tvTasbeesCounterView1.setText("تعداد سبحان الله = " + counterArr.get(index));
+                    break;
+                case 1:
+                    binding.tvTasbeesCounterView2.setText("تعداد الحمد لله = " + counterArr.get(index));
+                    break;
+                case 2:
+                    binding.tvTasbeesCounterView3.setText("تعداد استغفر الله = " + counterArr.get(index));
+                    break;
+                case 3:
+                    binding.tvTasbeesCounterView4.setText("تعداد لا إله إلا الله = " + counterArr.get(index));
+                    break;
+                case 4:
+                    binding.tvTasbeesCounterView5.setText("تعداد سبحان الله العظيم = " + counterArr.get(index));
+                    break;
+            }
+        int totalCount=0;
+        for (int i = 0; i < spinnerValues.length; i++) {
+            totalCount+=(counterArr.get(i));
+        }
+        binding.tvTasbeesTotalView.setText("المجموع = " +totalCount);
+    }
+
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         index = position;
         counter = counterArr.get(index);
-        binding.tvTasbeesCounterView.setText("العدد = " + counterArr.get(index));
+        setTextsvalues(index);
 //        Toast.makeText(getContext(), "Selected User: " + spinnerValues[position] + index, Toast.LENGTH_SHORT).show();
     }
 
